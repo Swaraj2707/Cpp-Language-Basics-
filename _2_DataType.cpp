5c2fa861f56c99cabad53c344ea9a03aaf5faa8d
Swaraj Singh <90082634+Swaraj2707@users.noreply.github.com>
2023-02-22 20:00:53 +0530

    Add files via upload

---
+++ _2_DataType.cpp
@@ -0,0 +1,11 @@
+// Data types are int, float, char etc.
+#include<iostream>
+using namespace std;
+
+int main(){
+    int marksInMaths = 98;
+    cout<<"Marks in maths is "<< marksInMaths; // this will print 'Marks in maths is 98'
+
+    char a[] = "Swaraj";
+    cout<<"Good Morning "<< a;
+}
