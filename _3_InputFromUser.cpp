c490b5629e138f232c425f56231fcac527c638a7
Swaraj Singh <90082634+Swaraj2707@users.noreply.github.com>
2023-02-22 20:01:14 +0530

    Add files via upload

---
+++ _3_InputFromUser.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a,b;
+    cout<<"Enter value of a \n";  
+    cin>>a;
+
+    cout<<"Enter value of b \n";   
+    cin>>b;
+
+    cout<<"The sum of a and b is "<<a+b;
+
+}
