e1e321bf72a8431dbb8fbfe9d36a46f701b1bd9c
Swaraj Singh <90082634+Swaraj2707@users.noreply.github.com>
2023-02-22 20:02:08 +0530

    Add files via upload

---
+++ _4_Conditional.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter your age\n";
+    cin>>age;
+
+    if (age>=18)
+    {
+        cout<<"You can vote";
+    }
+    else
+    {
+        cout<<"You cannot vote";
+    }
+    
+    
+}
